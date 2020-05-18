af4b05f0cd93df590dddcc5969c09f1312064889
thiagoac33 <aquino.thiago33@gmail.com>
2020-05-17 21:05:06 -0300

    first commit on repository

---
+++ Trabalho Prático/main.js
@@ -0,0 +1,7 @@
+function changeColor(){
+    let red = document.getElementById('red').value; 
+    let green = document.getElementById('green').value;
+    let blue = document.getElementById('blue').value;
+
+    document.getElementById('colorSelector').style.backgroundColor = `rgb(${red},${green},${blue})`
+}
