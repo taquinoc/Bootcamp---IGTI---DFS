33d0f7ab8a0997814778e02ce21c9deeb053a9ce
thiagoac33 <aquino.thiago33@gmail.com>
2020-05-31 21:10:11 -0300

    pausa para jogar o cszin

--- modulo02/trabalhoPratico/index.js
+++ modulo02/trabalhoPratico/index.js
@@ -5,7 +5,8 @@ const statesJson = require('./files/Estados.json')
 const citiesJson = require('./files/Cidades.json')
 
 
-/**1. Implementar um método que irá criar um arquivo JSON para cada estado representado no arquivo Estados.json, e o seu conteúdo será um array das cidades pertencentes aquele estado, de acordo com o arquivo Cidades.json. O nome do arquivo deve ser o UF do estado, por exemplo: MG.json. */
+/**1.
+ *  Implementar um método que irá criar um arquivo JSON para cada estado representado no arquivo Estados.json, e o seu conteúdo será um array das cidades pertencentes aquele estado, de acordo com o arquivo Cidades.json. O nome do arquivo deve ser o UF do estado, por exemplo: MG.json. */
 
 const statesCities = []
 statesJson.forEach((state, index) => {
@@ -17,34 +18,102 @@ statesJson.forEach((state, index) => {
     })
 })
 
-statesCities.forEach(element => {
-    fs.writeFile(`./states/${element.state}.json`, `[${element.cities}]`,  function(err){
-        if(err) throw err;
-        // console.log('complete')
-    })
-})
+// statesCities.forEach(element => {
+//     fs.writeFile(`./states/${element.state}.json`, `[${element.cities}]`,  function(err){
+//         if(err) throw err;
+//         // console.log('complete')
+//     })
+// })
 
-/**2. Criar um método que recebe como parâmetro o UF do estado, realize a leitura do arquivo JSON correspondente e retorne a quantidade de cidades daquele estado. */
+/**2.
+ *  Criar um método que recebe como parâmetro o UF do estado, realize a leitura do arquivo JSON correspondente e retorne a quantidade de cidades daquele estado. */
 
-const countCities = (stateName) => {
+// const countCities = (stateName) => {
 
-    const stateData = fs.readFileSync(`./states/${stateName}.json`)
-    const stateJson = JSON.parse(stateData)
-    let totalCities = stateJson.length
+//     const stateData = fs.readFileSync(`./states/${stateName}.json`)
+//     const stateJson = JSON.parse(stateData)
+//     let totalCities = stateJson.length
 
-    return totalCities
-}
+//     return totalCities
+// }
 
-console.log(countCities('MG'))
+// console.log(countCities('MG'))
 
 /**
  * 3.
  * Criar um método que imprima no console um array com o UF dos cinco estados que mais possuem cidades, seguidos da quantidade, em ordem decrescente. Utilize o método criado no tópico anterior. Exemplo de impressão: [“UF - 93”, “UF - 82”, “UF - 74”, “UF - 72”, “UF - 65”]
  */
 
-const countAllCities = () => {
 
+// const countAllCities = (dataStates) => {
+//     const allStates = []
+//     dataStates.forEach(element => {
+//         const stateData = fs.readFileSync(`./states/${element.Sigla}.json`)
+//         const stateJson = JSON.parse(stateData)
+//         let totalCities = stateJson.length
+
+//         allStates.push({UF: `${element.Sigla}`, total: totalCities})
+//     })
+
+//     const sortedArray = allStates.sort(function(a,b) {
+//         return b.total - a.total
+//     })
+
+//     const slicedArray = sortedArray.slice(0,5)
     
+//     return slicedArray
+// }
 
-    return totalCities
+// console.log(countAllCities(statesJson))
+
+
+/** 4. 
+ * Criar um método que imprima no console um array com o UF dos cinco estados que menos possuem cidades, seguidos da quantidade, em ordem decrescente. Utilize o método criado no tópico anterior. Exemplo de impressão: [“UF - 30”, “UF - 27”, “UF - 25”, “UF - 23”, “UF - 21”] */
+
+// const countAllCities = (dataStates) => {
+//     const allStates = []
+//     dataStates.forEach(element => {
+//         const stateData = fs.readFileSync(`./states/${element.Sigla}.json`)
+//         const stateJson = JSON.parse(stateData)
+//         let totalCities = stateJson.length
+
+//         allStates.push({UF: `${element.Sigla}`, total: totalCities})
+//     })
+
+//     const sortedArray = allStates.sort(function(a,b) {
+//         return a.total - b.total
+//     })
+
+//     const slicedArray = sortedArray.slice(0,5)
+    
+//     return slicedArray
+// }
+
+// console.log(countAllCities(statesJson))
+
+/**5.
+ * Criar um método que imprima no console um array com a cidade de maior nome de cada estado, seguida de seu UF. Em caso de empate, considerar a ordem alfabética para ordená-los e então retornar o primeiro. Por exemplo: [“Nome da Cidade – UF”, “Nome da Cidade – UF”, ...].
+ */
+
+
+const checkLengthNameAndOrderByAlphabetic = (dataStatesAndCities) => {
+    const allCitiesAndName = []
+    dataStatesAndCities.forEach(allCities => {
+        allCities.cities.forEach(eachCities => {
+            let citiesParsed = JSON.parse(eachCities)
+
+            allCitiesAndName.push({UF: allCities.state, nameLength: citiesParsed.Nome.length, name: citiesParsed.Nome })
+        })
+    })
+
+    const sorteddCitiesNames =  allCitiesAndName.sort(function(a,b) {
+        return b.nameLength - a.nameLength
+    })
+
+    sorteddCitiesNames.forEach(element =>{
+        
+    })
+    return  filteredCitiesNames
 }
+
+console.log(checkLengthNameAndOrderByAlphabetic(statesCities))
